07ae38f43f29d7ee0ab855ab4559fa984026efc6
Scott Finlay <scott.p.finlay@gmail.com>
2023-07-30 12:06:24 +0200

    Inferring http method correctly in case of --data

--- curl/method.go
+++ curl/method.go
@@ -3,9 +3,18 @@ package curl
 const methodHeader = "-X"
 
 func GetMethod(params map[string][]string) string {
-	method := "GET"
+	method := ""
 	if len(params[methodHeader]) == 1 {
 		method = params[methodHeader][0]
+	}
+
+	// infer default method
+	if method == "" {
+		if _, ok := GetBody(params); ok {
+			method = "POST"
+		} else {
+			method = "GET"
+		}
 	}
 	return method
 }
